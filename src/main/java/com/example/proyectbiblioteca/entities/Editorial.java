c00eed133e067ec4b44571ebffc3de476d4ab837
Luis Florez <braian1k3@gmail.com>
2023-05-19 13:22:46 -0500

    Finalizar la clase Editorial con sus respectivas descripciones.

--- src/main/java/com/example/proyectbiblioteca/entities/Editorial.java
+++ src/main/java/com/example/proyectbiblioteca/entities/Editorial.java
@@ -1,5 +1,24 @@
 package com.example.proyectbiblioteca.entities;
 
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Size;
+import lombok.*;
+import org.springframework.validation.annotation.Validated;
+
+@Setter
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Validated
+@Table(name = "editorial")
 public class Editorial {
-    
+
+    @Id
+    @Column(name = "nombre", nullable = false, length = 30)
+    @Size(min = 2, max = 30)
+    private String nombre;
+    @Column(name = "descripcion", nullable = false, length = 300)
+    @Size(max = 300)
+    private String descripcion;
 }
