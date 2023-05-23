4c16a6b8ad67c5c2ae2c5b27aefed4287f0b08cb
Luis Florez <braian1k3@gmail.com>
2023-05-23 00:40:54 -0500

    Finalizar la clase de EditorialService con servicios: traer todos los editoriales, traer un editorial, guardar un editorial, actualizar un editorial y borrar un editorial.

---
+++ src/main/java/com/example/proyectbiblioteca/services/EditorialService.java
@@ -0,0 +1,45 @@
+package com.example.proyectbiblioteca.services;
+
+import com.example.proyectbiblioteca.entities.Editorial;
+import com.example.proyectbiblioteca.repositories.EditorialRepository;
+import lombok.AllArgsConstructor;
+
+import java.util.List;
+import java.util.Optional;
+
+@AllArgsConstructor
+public class EditorialService {
+
+    private EditorialRepository editorialRepository;
+
+    public List<Editorial> getAllEditorial() {
+        return editorialRepository.findAll();
+    }
+
+    public Editorial getEditorial(String nombre) {
+        return editorialRepository.findById(nombre).orElse(null);
+    }
+
+    public Editorial saveEditorial(Editorial editorial) {
+        return editorialRepository.save(editorial);
+    }
+
+    public Editorial updateEditorial(Editorial editorial, String nombre) {
+        return  editorialRepository.findById(nombre)
+                .map(
+                    data -> {
+                        data.setNombre(editorial.getNombre());
+                        data.setDescripcion(editorial.getDescripcion());
+                        return editorialRepository.save(data);
+                    }
+                ).orElse(null);
+    }
+
+    public boolean deleteEditorial(String nombre) {
+        if (editorialRepository.findById(nombre).equals(Optional.empty())) {
+            return false;
+        }
+        editorialRepository.deleteById(nombre);
+        return true;
+    }
+}
