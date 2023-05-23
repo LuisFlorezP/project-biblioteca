e7a55d73fed0aa087858799b46cdf7fe511ec613
Luis Florez <braian1k3@gmail.com>
2023-05-23 00:40:23 -0500

    Remover unos import dentro del repository que no se están usando.

--- src/main/java/com/example/proyectbiblioteca/repositories/EditorialRepository.java
+++ src/main/java/com/example/proyectbiblioteca/repositories/EditorialRepository.java
@@ -2,9 +2,8 @@ package com.example.proyectbiblioteca.repositories;
 
 import com.example.proyectbiblioteca.entities.Editorial;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.http.ResponseEntity;
 
-import java.util.List;
+import java.util.Optional;
 
 public interface EditorialRepository extends JpaRepository<Editorial, String> {
 }
