7a6bd1e1357edaa6febd67cf6088ffba7ae43d28
Luis Florez <braian1k3@gmail.com>
2023-05-24 02:35:09 -0500

    Agregar otro país a la tabla Pais.

--- src/main/resources/paisRegisters.sql
+++ src/main/resources/paisRegisters.sql
@@ -4,7 +4,8 @@ use editorial;
 insert into pais (nombre) values 
 ('Brazil'), ('Francia'), ('Peru'), ('Rusia'), ('Inglaterra'), 
 ('España'), ('Portugal'), ('Ecuador'), ('Chile'), ('Bolivia'), 
-('EE.UU'), ('México'), ('Argentina'), ('Alemania'), ('Colombia');
+('EE.UU'), ('México'), ('Argentina'), ('Alemania'), ('Colombia'), 
+('Reino Unido');
 
 ----------------- VER TODOS LOS REGISTROS DE PAIS -----------------
 select * from pais;
