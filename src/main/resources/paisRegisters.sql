fd7ef4fca67f26139430713fd44220a683bc6912
Luis Florez <braian1k3@gmail.com>
2023-05-30 16:18:05 -0500

    Alterar archivo sql de los registros para paises.

--- src/main/resources/paisRegisters.sql
+++ src/main/resources/paisRegisters.sql
@@ -1,11 +1,11 @@
 use editorial;
 
 ------------------ INSERTAR REGISTROS DE PAISES ------------------
-insert into pais (nombre) values 
+insert into pais_entity (nombre) values 
 ('Brazil'), ('Francia'), ('Peru'), ('Rusia'), ('Inglaterra'), 
 ('España'), ('Portugal'), ('Ecuador'), ('Chile'), ('Bolivia'), 
 ('EE.UU'), ('México'), ('Argentina'), ('Alemania'), ('Colombia'), 
 ('Reino Unido');
 
 ----------------- VER TODOS LOS REGISTROS DE PAIS -----------------
-select * from pais;
+select * from pais_entity;
