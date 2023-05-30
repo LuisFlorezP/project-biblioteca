22d21a9bbdd24e9eee277724bb7de36e08706c88
Luis Florez <braian1k3@gmail.com>
2023-05-30 16:23:11 -0500

    Crear la entidad de Ubicacion, con sus atributos y detalles.

---
+++ src/main/java/com/example/proyectbiblioteca/entities/Ubicacion.java
@@ -0,0 +1,19 @@
+package com.example.proyectbiblioteca.entities;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "ubicacion_entity")
+public class Ubicacion {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+    @Column(name = "piso", nullable = false)
+    private String piso;
+    @Column(name = "salon", nullable = false)
+    private String salon;
+    @Column(name = "estante", nullable = false)
+    private String estante;
+}
