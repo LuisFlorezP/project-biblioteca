2360753b04a4f93ff432daf4ac24624ad198865c
Luis Florez <braian1k3@gmail.com>
2023-05-31 09:50:20 -0500

    Crear la interface de CategoriaRepository para extender JpaRepository.

---
+++ src/main/java/com/example/proyectbiblioteca/repositories/CategoriaRepository.java
@@ -0,0 +1,9 @@
+package com.example.proyectbiblioteca.repositories;
+
+import com.example.proyectbiblioteca.entities.Categoria;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CategoriaRepository extends JpaRepository<Categoria, Integer> {
+}
