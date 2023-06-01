9d1a2c87f4b75a53f94635b487dfb316fa27dc7b
Luis Florez <braian1k3@gmail.com>
2023-06-01 17:29:35 -0500

    Generar el DTO de la entidad Ubicacion: Location.

---
+++ src/main/java/com/example/proyectbiblioteca/dto/Location.java
@@ -0,0 +1,13 @@
+package com.example.proyectbiblioteca.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Setter
+@Getter
+public class Location {
+
+    private String floor;
+    private String lounge;
+    private String shelf;
+}
