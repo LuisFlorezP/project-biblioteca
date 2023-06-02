201798fcaef49bd8072747f4e4dab53c20eb6b8a
Luis Florez <braian1k3@gmail.com>
2023-06-02 02:24:36 -0500

    Generar el DTO de la entidad Categoria: Category.

---
+++ src/main/java/com/example/proyectbiblioteca/dto/Category.java
@@ -0,0 +1,12 @@
+package com.example.proyectbiblioteca.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Setter
+@Getter
+public class Category {
+
+    private String name;
+    private String description;
+}
