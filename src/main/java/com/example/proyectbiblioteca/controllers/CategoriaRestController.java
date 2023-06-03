e791858c0f91caea4ae4f566e0c03c99c8a3a523
Luis Florez <braian1k3@gmail.com>
2023-06-02 22:11:13 -0500

    Finalizar la transición del servicio CategoriaService y del controlador CategoriaRestController con la ayuda de mappers para el DTO de Category.

--- src/main/java/com/example/proyectbiblioteca/controllers/CategoriaRestController.java
+++ src/main/java/com/example/proyectbiblioteca/controllers/CategoriaRestController.java
@@ -1,13 +1,11 @@
 package com.example.proyectbiblioteca.controllers;
 
-import com.example.proyectbiblioteca.entities.Categoria;
-import com.example.proyectbiblioteca.entities.Ubicacion;
+import com.example.proyectbiblioteca.dto.Category;
 import com.example.proyectbiblioteca.services.CategoriaService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-
 import java.util.List;
 
 @RestController
@@ -18,35 +16,39 @@ public class CategoriaRestController {
     private CategoriaService categoriaService;
 
     @GetMapping("/")
-    public ResponseEntity<List<Categoria>> getAllCategories() {
+    public ResponseEntity<List<Category>> getAllCategories() {
         return new ResponseEntity<>(categoriaService.getAllCategories(), HttpStatus.OK);
     }
 
     @GetMapping("/{id}")
-    public ResponseEntity<Categoria> getCategory(@PathVariable Long id) {
-        return categoriaService.getCategory(id)
-                .map(categoria -> new ResponseEntity<>(categoria, HttpStatus.OK))
-                .orElse(new ResponseEntity<>(HttpStatus.NOT_FOUND));
+    public ResponseEntity<Category> getCategory(@PathVariable Long id) {
+        Category category = categoriaService.getCategory(id);
+        if (category != null) {
+            return new  ResponseEntity<>(category, HttpStatus.OK);
+        }
+        return ResponseEntity.notFound().build();
     }
 
     @PostMapping("/")
-    public ResponseEntity<Categoria> saveCategory(@RequestBody Categoria categoria) {
-        return categoriaService.saveCategory(categoria)
-                .map(data -> new ResponseEntity<>(data, HttpStatus.CREATED))
-                .orElse(new ResponseEntity<>(HttpStatus.BAD_REQUEST));
+    public ResponseEntity<Category> saveCategory(@RequestBody Category category) {
+        Category data = categoriaService.saveCategory(category);
+        if (data != null) {
+            return new ResponseEntity<>(data, HttpStatus.CREATED);
+        }
+        return ResponseEntity.badRequest().build();
     }
 
     @PutMapping("/{id}")
-    public ResponseEntity<Categoria> updateCategory(@RequestBody Categoria categoria, @PathVariable Long id) {
-        Categoria data = categoriaService.updateCategory(categoria, id);
+    public ResponseEntity<Category> updateCategory(@RequestBody Category category, @PathVariable Long id) {
+        Category data = categoriaService.updateCategory(category, id);
         if (data != null) {
             return new ResponseEntity<>(data, HttpStatus.OK);
         }
-        return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        return ResponseEntity.notFound().build();
     }
 
     @DeleteMapping("/{id}")
-    public ResponseEntity deleteCategory(@PathVariable Long id) {
+    public ResponseEntity<Category> deleteCategory(@PathVariable Long id) {
         if (categoriaService.deleteCategory(id)) {
             return ResponseEntity.noContent().build();
         }
--- src/main/java/com/example/proyectbiblioteca/services/CategoriaService.java
+++ src/main/java/com/example/proyectbiblioteca/services/CategoriaService.java
@@ -1,11 +1,12 @@
 package com.example.proyectbiblioteca.services;
 
+import com.example.proyectbiblioteca.dto.Category;
 import com.example.proyectbiblioteca.entities.Categoria;
+import com.example.proyectbiblioteca.mappers.CategoryMapper;
 import com.example.proyectbiblioteca.repositories.CategoriaRepository;
 import com.example.proyectbiblioteca.validations.GenerateValidation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-
 import java.util.List;
 import java.util.Optional;
 
@@ -14,38 +15,41 @@ public class CategoriaService extends GenerateValidation {
 
     @Autowired
     private CategoriaRepository categoriaRepository;
+    @Autowired
+    private CategoryMapper categoryMapper;
 
-    public List<Categoria> getAllCategories() {
-        return categoriaRepository.findAll();
+    public List<Category> getAllCategories() {
+        return categoryMapper.toCategories(categoriaRepository.findAll());
     }
 
-    public Optional<Categoria> getCategory(Long id) {
-        return categoriaRepository.findById(id);
+    public Category getCategory(Long id) {
+        return categoriaRepository.findById(id)
+                .map(categoria -> categoryMapper.toCategory(categoria))
+                .orElse(null);
+
     }
 
-    public Optional<Categoria> saveCategory(Categoria categoria) {
-        return categoriaRepository.findByNombre(categoria.getNombre())
-                .map(
-                        data -> {
-                            if (verificarDescripcionCategoria(categoria.getDescripcion().length())) {
-                                return Optional.<Categoria>empty();
-                            }
-                            return Optional.of(categoriaRepository.save(categoria));
-                        }
-                ).orElse(null);
-    }
-
-    public Categoria updateCategory(Categoria categoria, Long id) {
-        Categoria search = categoriaRepository.findByNombre(categoria.getNombre()).get();
-        if (search.getNombre().equals(categoria.getNombre()) || verificarDescripcionCategoria(categoria.getDescripcion().length())) {
+    public Category saveCategory(Category category) {
+        Optional<Categoria> categoriaNombre = categoriaRepository.findByNombre(category.getName());
+        if (categoriaNombre.isPresent() || verificarDescripcionCategoria(category.getDescription().length())) {
             return null;
         }
+        Categoria data = categoriaRepository.save(categoryMapper.toCategoria(category));
+        return categoryMapper.toCategory(data);
+    }
+
+    public Category updateCategory(Category category, Long id) {
         return  categoriaRepository.findById(id)
                 .map(
                         data -> {
-                            data.setNombre(categoria.getNombre());
-                            data.setDescripcion(categoria.getDescripcion());
-                            return categoriaRepository.save(data);
+                            Optional<Categoria> search = categoriaRepository.findByNombre(category.getName());
+                            if ((search.isPresent() && !search.get().getNombre().equals(category.getName())) || verificarDescripcionCategoria(category.getDescription().length())) {
+                                return null;
+                            }
+                            data.setNombre(category.getName());
+                            data.setDescripcion(category.getDescription());
+                            Categoria categoria = categoriaRepository.save(data);
+                            return categoryMapper.toCategory(categoria);
                         }
                 ).orElse(null);
     }
