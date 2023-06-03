79ace0d653f8398e73145788bb3ebb6e7ddf8553
Luis Florez <braian1k3@gmail.com>
2023-06-02 23:16:42 -0500

    Generar el mapper del DTO Author: AuthorMapper.

---
+++ src/main/java/com/example/proyectbiblioteca/mappers/AuthorMapper.java
@@ -0,0 +1,30 @@
+package com.example.proyectbiblioteca.mappers;
+
+import com.example.proyectbiblioteca.dto.Author;
+import com.example.proyectbiblioteca.entities.Autor;
+import org.mapstruct.InheritInverseConfiguration;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.Mappings;
+
+import java.util.List;
+
+@Mapper(componentModel = "spring")
+public interface AuthorMapper {
+
+    @Mappings({
+            @Mapping(source = "nombre", target = "name"),
+            @Mapping(source = "apellido", target = "lastName"),
+            @Mapping(source = "pseudonimo", target = "pseudonym")
+    })
+    Author toAuthor(Autor autor);
+    List<Author> toAuthors(List<Autor> autors);
+
+    @InheritInverseConfiguration
+    @Mappings({
+            @Mapping(target = "id", ignore = true),
+            @Mapping(target = "nacionalidad", ignore = true)
+    })
+    Autor toAutor(Author author);
+    List<Autor> toAutors(List<Author> authors);
+}
