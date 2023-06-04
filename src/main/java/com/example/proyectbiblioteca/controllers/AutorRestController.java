edeeebcab791e5d165d991de2b87f3fec1773e8c
Luis Florez <braian1k3@gmail.com>
2023-06-04 12:57:36 -0500

    Finalizar la transición del servicio AutorService y del controlador AutorRestController con la ayuda de mappers para el DTO de Author.

--- src/main/java/com/example/proyectbiblioteca/controllers/AutorRestController.java
+++ src/main/java/com/example/proyectbiblioteca/controllers/AutorRestController.java
@@ -1,8 +1,10 @@
 package com.example.proyectbiblioteca.controllers;
 
+import com.example.proyectbiblioteca.dto.Author;
 import com.example.proyectbiblioteca.entities.Autor;
 import com.example.proyectbiblioteca.services.AutorService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -17,39 +19,39 @@ public class AutorRestController {
     private AutorService autorService;
 
     @GetMapping("/")
-    public ResponseEntity<List<Autor>> getAllAutors() {
+    public ResponseEntity<List<Author>> getAllAutors() {
         return ResponseEntity.ok(autorService.getAllAutor());
     }
 
     @GetMapping("/{id}")
-    public ResponseEntity<Optional<Autor>> getAutor(@PathVariable Long id) {
-        Optional<Autor> autor = autorService.getAutor(id);
-        if (autor.equals(Optional.empty())) {
-            return ResponseEntity.notFound().build();
+    public ResponseEntity<Author> getAutor(@PathVariable Long id) {
+        Author author = autorService.getAutor(id);
+        if (author != null) {
+            return new ResponseEntity<>(author, HttpStatus.OK);
         }
-        return ResponseEntity.ok(autor);
+        return ResponseEntity.notFound().build();
     }
 
     @PostMapping("/")
-    public ResponseEntity<Autor> saveAutor(@RequestBody Autor autor) {
-        Optional<Autor> data = autorService.saveAutor(autor);
-        if (data.isEmpty()) {
-            return ResponseEntity.badRequest().build();
+    public ResponseEntity<Author> saveAutor(@RequestBody Autor autor) {
+        Author data = autorService.saveAutor(autor);
+        if (data != null) {
+            return new ResponseEntity<>(data, HttpStatus.CREATED);
         }
-        return ResponseEntity.ok(autor);
+        return ResponseEntity.badRequest().build();
     }
 
     @PutMapping("/{id}")
-    public ResponseEntity<Autor> updateAutor(@RequestBody Autor autor, @PathVariable Long id) {
-        Autor data = autorService.updateAutor(autor, id);
+    public ResponseEntity<Author> updateAutor(@RequestBody Autor autor, @PathVariable Long id) {
+        Author data = autorService.updateAutor(autor, id);
         if (data != null) {
-            return ResponseEntity.ok(data);
+            return new ResponseEntity<>(data, HttpStatus.OK);
         }
         return ResponseEntity.notFound().build();
     }
 
     @DeleteMapping("/{id}")
-    public ResponseEntity<Autor> deleteAutor(@PathVariable Long id) {
+    public ResponseEntity<Author> deleteAutor(@PathVariable Long id) {
         if (autorService.deleteAutor(id)) {
             return ResponseEntity.noContent().build();
         }
--- src/main/java/com/example/proyectbiblioteca/services/AutorService.java
+++ src/main/java/com/example/proyectbiblioteca/services/AutorService.java
@@ -1,6 +1,8 @@
 package com.example.proyectbiblioteca.services;
 
+import com.example.proyectbiblioteca.dto.Author;
 import com.example.proyectbiblioteca.entities.Autor;
+import com.example.proyectbiblioteca.mappers.AuthorMapper;
 import com.example.proyectbiblioteca.repositories.AutorRepository;
 import com.example.proyectbiblioteca.validations.GenerateValidation;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -13,35 +15,41 @@ public class AutorService extends GenerateValidation {
 
     @Autowired
     private AutorRepository autorRepository;
+    @Autowired
+    private AuthorMapper authorMapper;
 
-    public List<Autor> getAllAutor() {
-        return autorRepository.findAll();
+    public List<Author> getAllAutor() {
+        return authorMapper.toAuthors(autorRepository.findAll());
     }
 
-    public Optional<Autor> getAutor(Long id) {
-        return autorRepository.findById(id);
+    public Author getAutor(Long id) {
+        return autorRepository.findById(id)
+                .map(autor -> authorMapper.toAuthor(autor))
+                .orElse(null);
     }
 
-    public Optional<Autor> saveAutor(Autor autor) {
-        if (autorRepository.findByPseudonimo(autor.getPseudonimo()).isPresent() || verificarEmail(autor.getEmail()) || verificarNombreApellidoPseudonimo(autor) || verificarNacionalidad(autor.getNacionalidad())) {
-            return Optional.empty();
-        }
-        return Optional.of(autorRepository.save(autor));
-    }
-
-    public Autor updateAutor(Autor autor, Long id) {
-        Optional<Autor> autorDB = autorRepository.findByPseudonimo(autor.getPseudonimo());
-        if (autorDB.isPresent() || verificarEmail(autor.getEmail()) || autorDB.get().getPseudonimo().equals(autor.getPseudonimo()) || verificarNombreApellidoPseudonimo(autor)) {
+    public Author saveAutor(Autor autor) {
+        Optional<Autor> autorPseudonimo = autorRepository.findByPseudonimo(autor.getPseudonimo());
+        if (autorPseudonimo.isPresent() || verificarEmail(autor.getEmail()) || verificarNombreApellidoPseudonimo(autor) || verificarNacionalidad(autor.getNacionalidad())) {
             return null;
         }
+        return authorMapper.toAuthor(autorRepository.save(autor));
+    }
+
+    public Author updateAutor(Autor autor, Long id) {
         return  autorRepository.findById(id)
                 .map(
                         data -> {
+                            Optional<Autor> search = autorRepository.findByPseudonimo(autor.getPseudonimo());
+                            if (search.isPresent() && !search.get().getPseudonimo().equals(autor.getPseudonimo()) || verificarEmail(autor.getEmail()) || verificarNombreApellidoPseudonimo(autor) || verificarNacionalidad(autor.getNacionalidad())) {
+                                return null;
+                            }
                             data.setNombre(autor.getNombre());
                             data.setApellido(autor.getApellido());
                             data.setPseudonimo(autor.getPseudonimo());
+                            data.setNacionalidad(autor.getNacionalidad());
                             data.setEmail(autor.getEmail());
-                            return autorRepository.save(data);
+                            return authorMapper.toAuthor(autorRepository.save(data));
                         }
                 ).orElse(null);
     }
