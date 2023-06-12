b5a6ae5aac92249b25234021a9c60e104af3dce1
Luis Florez <braian1k3@gmail.com>
2023-06-12 12:16:55 -0500

    Generar el nuevo DTO DataAutorDTO, para trabajarlo a la hora de recibir servicios de guardar y actualizar, por ende también se generan mappers para DataAutorDTO - Autor.

---
+++ src/main/java/com/example/proyectbiblioteca/dto/autor/DataAutorDTO.java
@@ -0,0 +1,50 @@
+package com.example.proyectbiblioteca.dto.autor;
+
+public class DataAutorDTO extends AutorDTO {
+
+    private String nombre;
+    private String apellido;
+    private String pseudonimo;
+    private Long idNacionalidad;
+    private String email;
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public String getPseudonimo() {
+        return pseudonimo;
+    }
+
+    public void setPseudonimo(String pseudonimo) {
+        this.pseudonimo = pseudonimo;
+    }
+
+    public Long getIdNacionalidad() {
+        return idNacionalidad;
+    }
+
+    public void setIdNacionalidad(Long idNacionalidad) {
+        this.idNacionalidad = idNacionalidad;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
