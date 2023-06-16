7902e1d3ccee504cf6561a3ae9c287bb2ca05ebd
Luis Florez <braian1k3@gmail.com>
2023-06-16 13:16:24 -0500

    Renombrar el tipo de archivo de pruebas.txt a pruebas.json, además incluir nuevo archivo InformationsDB.sql para crear base de datos y ver los registros de todas las tablas.

---
+++ src/main/resources/InformationsDB.sql
@@ -0,0 +1,7 @@
+create database editorial
+use editorial
+select * from categoria_entity
+select * from ubicacion_entity
+select * from autor_entity
+select * from pais_entity
+select * from editorial_entity
