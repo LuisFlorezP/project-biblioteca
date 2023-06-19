d49c3926002a510a720ca3ccb8cbce24f0df9095
Luis Florez <braian1k3@gmail.com>
2023-06-19 17:13:18 -0500

    Generar el nuevo DTO RequestCategoryDTO, para trabajarlo a la hora de recibir servicios de guardar y actualizar, por ende también se generan mappers para RequestCategoryDTO - Category.

---
+++ src/main/java/com/example/proyectbiblioteca/dto/categoria/RequestCategoryDTO.java
@@ -0,0 +1,23 @@
+package com.example.proyectbiblioteca.dto.categoria;
+
+public class RequestCategoryDTO extends CategoryDTO {
+
+    private String nombre;
+    private String descripcion;
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+}
